cb3cd95820fc6524a867caf383aa87d7543d4b96
Geovanny <birdcode.py@gmail.com>
2024-03-13 21:20:55 -0500

    Creation of table of Plantae species richness

--- spatial_biodatabase_china.sql
+++ spatial_biodatabase_china.sql
@@ -4,5 +4,10 @@
 
 CREATE EXTENSION postgis;
 
+-- Kingdom: Plantae (GBIF records and species richness)
+
+SELECT scientific AS scientific_name, family, COUNT(DISTINCT id) AS gbif_records FROM gbif_shanghai
+WHERE kingdom = 'Plantae' GROUP BY scientific_name, family ORDER BY gbif_records DESC;
+
 
 
